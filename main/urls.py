e13e6918d110194a83cb1aa8e73a32a84f21b28e
Alexandr Borisov <sasha@ubuntuSasha>
2014-11-27 15:04:50 +0200

    correct contacts page url

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,5 @@ from main import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^contacts/', views.contacts, name='index')
+    url(r'^contacts/', views.contacts, name='contacts')
 )
